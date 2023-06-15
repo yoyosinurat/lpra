2971c9b22e68fc961493fdce0b6e767f8a579e43
ultramenid <malichamdan@gmail.com>
2023-06-15 19:46:40 +0700

    change about title

--- resources/views/frontends/about.blade.php
+++ resources/views/frontends/about.blade.php
@@ -6,7 +6,7 @@
     @include('partials.navdetail')
 
     <div class="max-w-4xl px-4  mx-auto sm:mt-12 mt-4">
-        <h1 class="text-bukanlepra font-semibold text-2xl">About Bukan LEPRA</h1>
+        <h1 class="text-bukanlepra font-semibold text-2xl">About Lokasi Prioritas Reforma Agraria</h1>
         <div class="prose max-w-none">
             {!! $data->content !!}
         </div>
